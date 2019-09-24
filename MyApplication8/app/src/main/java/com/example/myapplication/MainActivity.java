03abc6072bb90e3b76497629f00372acee7fac4b
FarewellYO <1498444061@qq.com>
2019-09-24 13:53:31 +0800

    Project2-android

---
+++ MyApplication8/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.mWebView);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+
+        mWebView.loadUrl("file:///android_asset/1User.html");
+
+        //测试远程的 mwebview.loadUrl("http://www.itxdl.cn");
+    }
+}
